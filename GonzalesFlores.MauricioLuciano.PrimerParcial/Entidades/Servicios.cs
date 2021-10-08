2e14847290453c44db654aea203a56b2d39ae9d2
Mauricio Gonzales <mgonzales@tesacom.net>
2021-10-08 20:17:49 -0300

    clase servicios creada

---
+++ GonzalesFlores.MauricioLuciano.PrimerParcial/Entidades/Servicios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Servicios
+    {
+        string identificador;
+        float costoDeUso;
+        public int minutos;
+        public bool activo;
+
+        public virtual string Identificador
+        {
+            set { this.identificador = value; }
+            get { return this.identificador; }
+        }
+        public virtual float CostoDeUso
+        {
+            set { this.costoDeUso = value; }
+            get { return this.costoDeUso; }
+        }
+        public abstract bool ValidarIdentificador(string code);
+        public float Costo(int minutos, string valorInicial)
+        {
+            int tiempo = 30;
+            float valor = float.Parse(valorInicial);
+
+            while (tiempo <= minutos)
+            {
+                valor = valor * 2;
+                minutos = minutos * 2;
+            }
+            return valor;
+        }
+        public abstract string Mostrar();
+    }
+}
